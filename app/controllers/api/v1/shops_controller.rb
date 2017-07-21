8a1e13aedca68e24403057bda9c56c288e9287a9
zach beecher <zachbeecher@gmail.com>
2017-07-21 11:14:20 -0700

    add show individual shop with id

--- app/controllers/api/v1/shops_controller.rb
+++ app/controllers/api/v1/shops_controller.rb
@@ -5,6 +5,11 @@ module Api
         shops = Shop.order('created_at DESC');
         render json: {status: 'SUCCESS', message:'Loaded shops', data:shops},status: :ok
       end
+
+      def show
+        shop = Shop.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Loaded shops', data:shop}, status: :ok
+      end
     end
   end
 end
